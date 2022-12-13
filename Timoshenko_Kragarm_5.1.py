f1ab8b0be11135bd673cbca3b0cec31f18edc539
jonas <jonas.peter@tuhh.de>
2022-12-13 13:34:13 +0100

    Neu

--- Timoshenko_Kragarm_5.1.py
+++ Timoshenko_Kragarm_5.1.py
@@ -18,19 +18,6 @@ from scipy.interpolate import splrep, splev
 import math
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-import scipy.integrate
-import sympy as sy
-import torch
-import torch.nn as nn
-from torch.autograd import Variable
-import scipy as sp
-import scipy.integrate as integrate
-from scipy.integrate import quad
-import scipy.special as special
-import matplotlib.pyplot as plt
-import numpy as np
-from scipy.interpolate import splrep, splev
-import math
 
 class Net(nn.Module):
     def __init__(self):
@@ -56,53 +43,67 @@ class Net(nn.Module):
         output = self.output_layer(layer7_out)
         return output
 
+##
+choice_load = input("Möchtest du ein State_Dict laden? y/n")
+if choice_load == 'y':
+    train=False
+    filename = input("Welches State_Dict möchtest du laden?")
+    net = Net()
+    net = net.to(device)
+    net.load_state_dict(torch.load('C:\\Users\\Administrator\\Desktop\\Uni\\Master\\Masterarbeit\\Timoshenko NN Kragarm 5.3\\saved_data\\'+filename))
+    net.eval()
+##
 # Hyperparameter
-learning_rate = 0.0035
-
-net_B = Net()
-net_S = Net()
-net_B = net_B.to(device)
-net_S = net_S.to(device)
+learning_rate = 0.01
+net_phi = Net()
+net_v = Net()
+net_phi = net_phi.to(device)
+net_v = net_v.to(device)
 mse_cost_function = torch.nn.MSELoss()  # Mean squared error
-optimizer = torch.optim.Adam([{'params': net_B.parameters()}, {'params': net_S.parameters()}], lr=learning_rate)
+optimizer = torch.optim.Adam([{'params': net_phi.parameters()}, {'params': net_v.parameters()}], lr=learning_rate)
 #Der Scheduler sorgt dafür, dass die Learning Rate auf einem Plateau mit dem factor multipliziert wird
 scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=200, verbose=True, factor= 0.8)
 
 # Definition der Parameter des statischen Ersatzsystems
 Lb = float(input('Länge des Kragarms [m]: '))
-EI = float(input('EI des Balkens [10^6 kNcm²]: '))
-A = float(input('Querschnittsfläche des Balkens [cm²]: '))
-G = float(input('Schubmodul des Balkens [GPa]: '))
-LFS = int(input('Anzahl Streckenlasten: '))
+E = 21  #float(input('E-Modul des Balkens [10^6 kNcm²]: '))
+h = 10  #float(input('Querschnittshöhe des Balkens [cm]: '))
+b = 10  #float(input('Querschnittsbreite des Balkens [cm]: '))
+A = h*b
+I = (b*h**3)/12
+EI = E*I*10**-3
+G = 80  #float(input('Schubmodul des Balkens [GPa]: '))
+LFS = 1  #int(input('Anzahl Streckenlasten: '))
 K = 5 / 6  # float(input(' Schubkoeffizient '))
 Ln = np.zeros(LFS)
 Lq = np.zeros(LFS)
 s = [None] * LFS
+normfactor = 10/(Lb**3/(K*A*G)+(11*Lb**5)/(120*EI))
 
 for i in range(LFS):
     # ODE als Loss-Funktion, Streckenlast
-    Ln[i] = float(input('Länge Einspannung bis Anfang der ' + str(i + 1) + '. Streckenlast [m]: '))
-    Lq[i] = float(input('Länge der ' + str(i + 1) + '. Streckenlast [m]: '))
-    s[i] = input(str(i + 1) + '. Streckenlast eingeben: ')
+    Ln[i] = 0#float(input('Länge Einspannung bis Anfang der ' + str(i + 1) + '. Streckenlast [m]: '))
+    Lq[i] = Lb#float(input('Länge der ' + str(i + 1) + '. Streckenlast [m]: '))
+    s[i] = str(normfactor)+"*x"#input(str(i + 1) + '. Streckenlast eingeben: ')
 
 def h(x, j):
     return eval(s[j])
 
 #Netzwerk System 1
-def f(x, net_B):
-    u = net_B(x)
+def f(x, net_phi):
+    u = net_phi(x)
     u_x = torch.autograd.grad(u, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     u_xx = torch.autograd.grad(u_x, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     u_xxx = torch.autograd.grad(u_xx, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     ode = 0
     for i in range(LFS):
-        ode += u_xxx + h(x - Ln[i], i)  * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
+        ode += u_xxx + h(x - Ln[i], i) * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
     return ode
 
 #Netzwerk für System 2
-def g(x, net_S, net_B):
-    u = net_S(x)
-    z = net_B(x)
+def g(x, net_v, net_phi):
+    u = net_v(x)
+    z = net_phi(x)
     u_x = torch.autograd.grad(u, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(u))[0]
     z_x = torch.autograd.grad(z, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(z))[0]
     z_xx = torch.autograd.grad(z_x, x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(z))[0]
@@ -111,6 +112,7 @@ def g(x, net_S, net_B):
 
 
 x = np.linspace(0, Lb, 1000)
+pt_x = torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=True).to(device), 1)
 qx = np.zeros(1000)
 for i in range(LFS):
     qx = qx + (h(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1) - Ln[i], i).cpu().detach().numpy()).squeeze() * (x <= (Ln[i] + Lq[i])) * (x >= Ln[i])
@@ -121,15 +123,17 @@ qxx = qx * x
 #M0 = M(0) = int(q(x)*x), über den ganzen Balken
 M0 = integrate.cumtrapz(qxx, x, initial=0)
 #Die nächsten Zeilen bis Iterationen geben nur die Biegelinie aus welche alle 10 Iterationen refreshed wird während des Lernens, man kann also den Lernprozess beobachten
-y1 = net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1)) #+ net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1))
+y1 = net_v(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1)) #+ net_phi(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device), 1))
 fig = plt.figure()
 plt.grid()
 ax = fig.add_subplot()
 ax.set_xlim([0, Lb])
 ax.set_ylim([-30, 0])
 line1, = ax.plot(x, y1.cpu().detach().numpy())
-
-iterations = 12000
+f_anal = (-1 / 120 * normfactor * pt_x ** 5 + 1 / 6 * Q0[-1] * pt_x ** 3 - M0[-1] / 2 * pt_x ** 2) / EI + (
+            1 / 6 * normfactor * (pt_x) ** 3 - Q0[-1] * pt_x) / (K * A * G)
+##
+iterations = 1000000
 for epoch in range(iterations):
     optimizer.zero_grad()  # to make the gradients zero
     x_bc = np.linspace(0, Lb, 500)
@@ -144,41 +148,41 @@ for epoch in range(iterations):
 
     pt_x_collocation = Variable(torch.from_numpy(x_collocation).float(), requires_grad=True).to(device)
     pt_all_zeros = Variable(torch.from_numpy(all_zeros).float(), requires_grad=False).to(device)
-    ode_B = f(pt_x_collocation, net_B)
-    ode_S = g(pt_x_collocation, net_S, net_B)
+    ode_phi = f(pt_x_collocation, net_phi)
+    ode_v = g(pt_x_collocation, net_v, net_phi)
 
     # Randbedingungen
-    net_bc_out_B = net_B(pt_x_bc)
-    net_bc_out_S = net_S(pt_x_bc)
+    net_bc_out_phi = net_phi(pt_x_bc)
+    net_bc_out_S = net_v(pt_x_bc)
     # ei --> Werte, die minimiert werden müssen
-    u_x_B = torch.autograd.grad(net_bc_out_B, pt_x_bc, create_graph=True, retain_graph=True,
-                                grad_outputs=torch.ones_like(net_bc_out_B))[0]
-    u_xx_B = torch.autograd.grad(u_x_B, pt_x_bc, create_graph=True, retain_graph=True,
-                                 grad_outputs=torch.ones_like(net_bc_out_B))[0]
-    u_xxx_B = torch.autograd.grad(u_xx_B, pt_x_bc, create_graph=True, retain_graph=True,
-                                  grad_outputs=torch.ones_like(net_bc_out_B))[0]
-    u_x_S = torch.autograd.grad(net_bc_out_S, pt_x_bc, create_graph=True, retain_graph=True,
-                                grad_outputs=torch.ones_like(net_bc_out_S))[0]
+    u_x_phi = torch.autograd.grad(net_bc_out_phi, pt_x_bc, create_graph=True, retain_graph=True,
+                                grad_outputs=torch.ones_like(net_bc_out_phi))[0]
+    u_xx_phi = torch.autograd.grad(u_x_phi, pt_x_bc, create_graph=True, retain_graph=True,
+                                 grad_outputs=torch.ones_like(net_bc_out_phi))[0]
+    u_xxx_phi = torch.autograd.grad(u_xx_phi, pt_x_bc, create_graph=True, retain_graph=True,
+                                  grad_outputs=torch.ones_like(net_bc_out_phi))[0]
+    u_x_v = torch.autograd.grad(net_bc_out_v, pt_x_bc, create_graph=True, retain_graph=True,
+                                grad_outputs=torch.ones_like(net_bc_out_v))[0]
 
     #RB für Netzwerk 1
-    e1_B = net_bc_out_B[0]
-    e2_B = u_x_B[0] + M0[-1]
-    e3_B = u_x_B[-1]
-    e4_B = u_xx_B[0] - Q0[-1]
-    e5_B = u_xx_B[-1]
+    e1_phi = net_bc_out_phi[0]
+    e2_phi = u_x_phi[0] + M0[-1]
+    e3_phi = u_x_phi[-1]
+    e4_phi = u_xx_phi[0] - Q0[-1]
+    e5_phi = u_xx_phi[-1]
 
 
     #RB für Netzwerk 2
-    e1_S = net_bc_out_S[0]
+    e1_v = net_bc_out_v[0]
 
     #Alle e's werden gegen 0-Vektor (pt_zero) optimiert.
 
-    mse_bc_B = mse_cost_function(e1_B, pt_zero) + mse_cost_function(e2_B, pt_zero) + mse_cost_function(e3_B, pt_zero) + mse_cost_function(e4_B, pt_zero) + mse_cost_function(e5_B, pt_zero)
-    mse_ode_B = mse_cost_function(ode_B, pt_all_zeros)
-    mse_bc_S = mse_cost_function(e1_S, pt_zero)
-    mse_ode_S = mse_cost_function(ode_S, pt_all_zeros)
+    mse_bc_phi = mse_cost_function(e1_phi, pt_zero) + 1/normfactor * mse_cost_function(e2_phi, pt_zero) + mse_cost_function(e3_phi, pt_zero) + 1/normfactor * mse_cost_function(e4_phi, pt_zero) + mse_cost_function(e5_phi, pt_zero)
+    mse_ode_phi = mse_cost_function(ode_phi, pt_all_zeros)
+    mse_bc_v = mse_cost_function(e1_v, pt_zero)
+    mse_ode_v = mse_cost_function(ode_v, pt_all_zeros)
 
-    loss = 3*mse_ode_S + 3*mse_ode_B + mse_bc_S + mse_bc_B
+    loss = 3*mse_ode_v + 3*mse_ode_phi + mse_bc_v + mse_bc_phi
 
     loss.backward()
     optimizer.step()
@@ -187,14 +191,29 @@ for epoch in range(iterations):
         if epoch % 10 == 9:
             print(epoch, "Traning Loss:", loss.data)
             plt.grid()
-            line1.set_ydata(net_B(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device),1)).cpu().detach().numpy() + net_S(torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=False).to(device),1)).cpu().detach().numpy())
+            net_out = net_v(pt_x)
+            net_out_v = net_out
+            net_out_v_cpu = net_out_v.cpu().detach().numpy()
+            err = torch.norm(net_out_v - f_anal, 2)
+            print(f'Error = {err}')
+            if err < 0.1 * Lb:
+                print(f"Die L^2 Norm des Fehlers ist {err}.\nStoppe Lernprozess")
+                break
+            line1.set_ydata(net_out_v_cpu)
             fig.canvas.draw()
             fig.canvas.flush_events()
+
+##
+if choice_load == 'n':
+    choice_save = input("Möchtest du die Netzwerkparameter abspeichern? y/n")
+    if choice_save == 'y':
+        filename = input("Wie soll das State_Dict heißen?")
+        torch.save(net.state_dict(),'C:\\Users\\Administrator\\Desktop\\Uni\\Master\\Masterarbeit\\Timoshenko NN Kragarm 5.3\\saved_data\\'+filename)
 ##
 pt_x = torch.unsqueeze(Variable(torch.from_numpy(x).float(), requires_grad=True).to(device), 1)
 
-pt_u_out_s = net_S(pt_x)
-pt_u_out = net_B(pt_x)
+pt_u_out_v = net_v(pt_x)
+pt_u_out = net_phi(pt_x)
 w_x = torch.autograd.grad(pt_u_out, pt_x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(pt_u_out))[0]
 w_xx = torch.autograd.grad(w_x, pt_x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(pt_u_out))[0]
 w_xxx = torch.autograd.grad(w_xx, pt_x, create_graph=True, retain_graph=True, grad_outputs=torch.ones_like(pt_u_out))[0]
